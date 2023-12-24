d2729e5779ddcbd97db21d226c5ee374df770853
Alexander Gnauck <gnauck@gmail.com>
2023-12-24 01:31:43 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { Toggle } from '@/components/ui/toggle'
+import IconUsers from './IconUsers.vue'
+import { ref } from 'vue'
+
+const toggleState = ref(true)
+</script>
+
+<template>
+  <Toggle variant="outline" size="lg" v-model:pressed="toggleState">
+    <IconUsers class="w-6 h-6"/>
+  </Toggle>
+</template>
